2818ceda35afe7a9ad6d96f582de68bf92ec5308
hadihm <hadialmumayez@gmail.com>
2020-03-25 00:36:54 +0200

    remove unnececery

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,3 @@
-console.log('client js is loaded')
-
-
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne= document.querySelector('#message-1')
